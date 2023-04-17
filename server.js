dd7e8f7d1700b5cbd3cd854b17c94abf491e9b46
Venka Tesh G <gvenkatesh225@gmail.com>
2023-04-17 13:42:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -45,12 +45,12 @@ wss.on('connection', function connection(ws) {
   })
 
 
-let port = 3000;
+let port = 6969;
 app.listen(port, function() {
     console.log(`Server is listening on ${port}!`)
   })
 
-port = 6969;
+port = 3000;
 server.listen(port, function() {
   console.log(`Server is listening on ${port}!`)
 })
